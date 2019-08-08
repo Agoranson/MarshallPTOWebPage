61267381a8ffc011bf78ea217cd1e76fd0c52c98
Agoranson <Goransonamanda@gmail.com>
2019-08-08 06:58:32 -0500

    Updated to add javascript functionality

---
+++ functionality.js
@@ -0,0 +1,9 @@
+//this will change the color of the marshall header 
+//so it can be seen on the yellow header 
+ 
+    var locationOfYellowDiv = 350; 
+    console.log(locationOfYellowDiv); 
+    //if(location > 40)
+    
+ document.getElementById("marshallheaderid").style.color = "green";  
+ 
